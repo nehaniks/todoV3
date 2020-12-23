f169646c49f865a8b1b506f88373978c8ab5e5dd
Neha Nikhat <nehanikhat18@gmail.com>
2020-12-23 16:37:43 +0530

    Display todos

---
+++ src/components/Todos.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <h1>{{ todo.title }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todos",
+    props: {
+        todos: {
+            required: true,
+            type: Object
+        }
+    }
+    // setup(props) {
+    //     let todos = []
+    //     return {
+
+    //     }
+    // }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="home">
-  
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Todos from "../components/Todos";
 
 export default {
   name: 'Home',
   components: {
+    Todos
+  },
+  setup() {
+    let todos = [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: true
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: true
+      }
+    ]
+    return {
+      todos
+    }
   }
 }
 </script>
